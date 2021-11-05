2e5bb926189dbc5d08c4f0cdfb3ef5a513febe52
Aleksandra Abrosimova <auabrosimova@gmail.com>
2021-11-05 22:01:03 +0300

    add settings and support json in tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 
 import Playground from './Playground';
+import settings from './settings.json';
+
 
 window.onload = function() {
+
+  const $start = document.querySelector('#' + settings["start"])
+  const $pause = document.querySelector('#' + settings["pause"])
+  const $left = document.querySelector('#' + settings["left"])
+  const $right = document.querySelector('#' + settings["right"])
+  const $rotate = document.querySelector('#' + settings["rotate"])
+  const $speed = document.querySelector('#' + settings["speed"])
+  const $score = document.querySelector('#' + settings["score"])
+
+
+  $start.addEventListener('click', () => {
+
+    alert("Start");
+
+  })
+
+
+
     const playground  = new Playground('tetris');
     const matrix: number[][] = [
       [0,0,0,0,0,1,0,0,0,0,0,0],
